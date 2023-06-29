400b2ce7828df75dc71f44f5b5befa7a5529df4a
Andres Marino <andresmarinolaiolo@hotmail.com.ar>
2023-06-28 23:58:30 -0300

    more env params

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,9 +3,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "amstore-ecommerce.firebaseapp.com",
-  projectId: "amstore-ecommerce",
-  storageBucket: "amstore-ecommerce.appspot.com",
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINSENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
